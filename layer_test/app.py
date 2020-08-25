c4aff9a45de8b7a1257c4c73fc7c01652dab2bae
rudrvij <rudrapaka@gmail.com>
2020-08-25 15:39:17 -0500

    tst

--- layer_test/app.py
+++ layer_test/app.py
@@ -36,7 +36,7 @@ def lambda_handler(event, context):
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "This is SAM Layer Test",
+            "message": "This is SAM Layer Test" + displayText(),
             # "location": ip.text.replace("\n", "")
         }),
     }
